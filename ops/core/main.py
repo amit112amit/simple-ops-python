1ee81b7c37619e3bbcb0b97808fd5472cff99f90
Amit Singh <amit112amit@g.ucla.edu>
2018-09-03 22:44:12 -0700

    Numba speed ups implemented.

--- ops/core/main.py
+++ ops/core/main.py
@@ -1,99 +1,171 @@
+from math import sqrt, sin, cos, exp
 import numpy as np
-from numba import jit
-from ops.core.meshmethods import triangulate, averageEdgeLength, getEdges
+from numba import njit
 from ops.core.rotations import get_orientations
 
-@jit(nopython=True,cache=True)
-def diffNRV( rv, Mij ):
+
+@njit(cache=True)
+def diffNRV(rv, Mij):
     """ Differentiate orientations wrt rotation vectors. """
     N = rv.shape[0]
+    Aaj = np.zeros((4, 3))
+    Bia = np.zeros((3, 4))
+    eye = np.array([[0.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0]])
     for i in range(N):
-        u = np.linalg.norm( rv[i] )
-        a = 0.5*u
-        ca = np.cos(a)
-        sau = np.sin(a)/u
-        q0 = ca
-        q1, q2, q3 = sau*rv[i]
-        Aaj = 2*np.array([q2,-q1,q0,q3,-q0,-q1,q0,q3,-q2,q1,q2,q3]).reshape(
-            (4,3))
-        Bia = np.zeros( (3,4) )
-        Bia[:,0] = -0.5*sau*rv[i]
-        Bia[:,1:] = sau*np.eye(3) + (0.5*ca - sau)*np.outer(rv[i],rv[i])/u**2
-        Mij[:,:,i] = np.dot( Bia, Aaj)
+        r0 = rv[i, 0]
+        r1 = rv[i, 1]
+        r2 = rv[i, 2]
 
-@jit(nopython=True,cache=True)
-def energyAndJacobian( x, conn, fvk=0.01, re=1.0, a=4.620981204 ):
+        u = sqrt(r0 * r0 + r1 * r1 + r2 * r2)
+        a = 0.5 * u
+        ca = cos(a)
+        sau = sin(a) / u
+
+        q0 = ca
+        q1 = sau * r0
+        q2 = sau * r1
+        q3 = sau * r2
+
+        Aaj[0, 0] = 2 * q2
+        Aaj[0, 1] = -2 * q1
+        Aaj[0, 2] = 2 * q0
+        Aaj[1, 0] = 2 * q3
+        Aaj[1, 1] = -2 * q0
+        Aaj[1, 2] = -2 * q1
+        Aaj[2, 0] = 2 * q0
+        Aaj[2, 1] = 2 * q3
+        Aaj[2, 2] = -2 * q2
+        Aaj[3, 0] = 2 * q1
+        Aaj[3, 1] = 2 * q2
+        Aaj[3, 2] = 2 * q3
+
+        Bia[0, 0] = -0.5 * sau * r0
+        Bia[1, 0] = -0.5 * sau * r1
+        Bia[2, 0] = -0.5 * sau * r2
+
+        fac = (0.5 * ca - sau) / u**2
+        for l in range(3):
+            for m in range(1, 4):
+                Bia[l, m] = sau * eye[l, m] + fac * rv[i, l] * rv[i, m-1]
+
+        for l in range(3):
+            for n in range(3):
+                Mij[i, l, n] = 0.0
+                for m in range(4):
+                    Mij[i, l, n] += Bia[l, m] * Aaj[m, n]
+
+
+@njit(cache=True)
+def energyAndJacobian(x, conn, fvk=0.01, re=1.0, a=4.620981204):
     """ Calculate energy and jacobian of the energy of the OPS shell. """
     # Prepare data arrays
-    NP = int( len(x) / 6 ) # Number of particles
-    energy = 0
-    X = x.reshape( (-1,3) )
-    jac = np.zeros_like( X )
-    jacX = jac[:NP,:]
-    jacR = jac[NP:,:]
+    energy = 0.0
+    NP = int(len(x) / 6)  # Number of particles
+    X = x.reshape((-1, 3))
+    jac_arr = np.zeros(NP*6)
+    jac = jac_arr.reshape((-1, 3))
+    jacX = jac[:NP, :]
+    jacR = jac[NP:, :]
 
     # Get the particle positions, rotation vectors and derivatives
-    pos = X[:NP,:]
-    rot = X[NP:,:]
-    ori = np.zeros_like( rot )
-    get_orientations( rot, ori )
-    dNdr = np.zeros( (3,3,NP) )
-    diffNRV( rot, dNdr )
+    pos = X[:NP, :]
+    rot = X[NP:, :]
+    ori = np.zeros_like(rot)
+    get_orientations(rot, ori)
+    dNdr = np.zeros((NP, 3, 3))
+    diffNRV(rot, dNdr)
+
+    rij = np.zeros(3)
+    rn = np.zeros(3)
+    m = np.zeros(3)
+    n = np.zeros(3)
+    dMdr = np.zeros(3)
+    dCdr = np.zeros(3)
+    dPhi_cVi = np.zeros(3)
+    dPhi_cVj = np.zeros(3)
+    dPhi_nVi = np.zeros(3)
+    dPhi_nVj = np.zeros(3)
 
     # Iterate over all edges from connectivity and calculate
     for z in range(conn.shape[0]):
+
         # Get the point ids
-        i, j = conn[z]
+        i = conn[z, 0]
+        j = conn[z, 1]
+
         # Get the positions and orientations and derivatives of normals
-        rij =  pos[j] - pos[i]
-        r = np.linalg.norm(rij)
-        rn = rij/r
-        m = ori[i] - ori[j]
-        n = ori[i] + ori[j]
-        M = dNdr[:,:,i]
-        N = dNdr[:,:,j]
-        n_dot_rn = np.dot(n,rn)
-        # Morse potential and derivatives
-        exp_1 = np.exp( -a*(r - re) )
+        for k in range(3):
+            rij[k] = pos[j, k] - pos[i, k]
+            m[k] = ori[i, k] - ori[j, k]
+            n[k] = ori[i, k] + ori[j, k]
+
+        r = sqrt(rij[0] * rij[0] + rij[1] * rij[1] + rij[2] * rij[2])
+        inv_r = 1.0 / r
+
+        n_dot_rn = 0.0
+        m_dot_m = 0.0
+        for k in range(3):
+            rn[k] = rij[k] * inv_r
+            n_dot_rn += n[k] * rn[k]
+            m_dot_m += m[k] * m[k]
+
+        # Morse potential and Co-circularity
+        exp_1 = exp(-a*(r - re))
         exp_2 = exp_1**2
-        dMdr = 2*a*(exp_1 - exp_2)*rn
-        # Co-normality potential and derivatives
-        dPhi_nVi = 2*np.dot(M,m)
-        dPhi_nVj = -2*np.dot(N,m)
-        # Co-circularity potential and derivatives
-        dCdr = 2*n_dot_rn*(n - n_dot_rn*rn)/r
-        dPhi_cVi = 2*n_dot_rn*np.dot(M,rn)
-        dPhi_cVj = 2*n_dot_rn*np.dot(N,rn)
+
+        # Morse and Co-circularity derivatives
+        fac = 2*a*(exp_1 - exp_2)
+        fac2 = 2 * n_dot_rn * inv_r
+        for k in range(3):
+            dMdr[k] = fac * rn[k]
+            dCdr[k] = fac2 * (n[k] - n_dot_rn * rn[k])
+
+        # Matrix vector scalar multiplications
+        #  Co-normality derivatives
+        #  Co-circularity potential derivatives
+        fac = 2 * n_dot_rn
+        for p in range(3):
+            dPhi_nVi[p] = 0.0
+            dPhi_nVj[p] = 0.0
+            dPhi_cVi[p] = 0.0
+            dPhi_cVj[p] = 0.0
+            for q in range(3):
+                dPhi_nVi[p] += 2.0 * dNdr[i, p, q] * m[q]
+                dPhi_nVj[p] -= 2.0 * dNdr[j, p, q] * m[q]
+                dPhi_cVi[p] += fac * dNdr[i, p, q] * rn[q]
+                dPhi_cVj[p] += fac * dNdr[j, p, q] * rn[q]
+
         # Total energy
-        energy += (exp_2 - 2*exp_1) + ( np.dot(m,m) + n_dot_rn**2 )/fvk
+        inv_fvk = 1.0 / fvk
+        energy += (exp_2 - 2*exp_1) + (m_dot_m + n_dot_rn**2) * inv_fvk
+
         # Total Derivatives of energy wrt xi, vi, vj
-        Dxi = -(dMdr + dCdr/fvk)
-        Dvi = (dPhi_nVi + dPhi_cVi)/fvk
-        Dvj = (dPhi_nVj + dPhi_cVj)/fvk
-        # Update the jacobian
-        jacX[i] += Dxi
-        jacX[j] += -Dxi
-        jacR[i] += Dvi
-        jacR[j] += Dvj
+        for k in range(3):
+            jacX[i, k] -= dMdr[k] + dCdr[k] * inv_fvk
+            jacX[j, k] += dMdr[k] + dCdr[k] * inv_fvk
+            jacR[i, k] += (dPhi_nVi[k] + dPhi_cVi[k]) * inv_fvk
+            jacR[j, k] += (dPhi_nVj[k] + dPhi_cVj[k]) * inv_fvk
 
-    return energy, jac.ravel()
+    return energy, jac_arr
 
 
-@jit(nopython=True,cache=True)
-def energy( x, conn, fvk=0.01, re=1.0, a=4.620981204 ):
+@njit(cache=True)
+def energy(x, conn, fvk=0.01, re=1.0, a=4.620981204):
     """ Calculate the more, normality and circularity energy. """
     # Prepare data arrays
-    NP = int( len(x) / 6 ) # Number of particles
+    NP = int(len(x) / 6)  # Number of particles
     morse = 0
     normality = 0
     circularity = 0
-    X = x.reshape( (-1,3) )
+    X = x.reshape((-1, 3))
 
     # Get the particle positions, rotation vectors and derivatives
-    pos = X[:NP,:]
-    rot = X[NP:,:]
-    ori = np.zeros_like( rot )
-    get_orientations( rot, ori )
+    pos = X[:NP, :]
+    rot = X[NP:, :]
+    ori = np.zeros_like(rot)
+    get_orientations(rot, ori)
 
     # Iterate over all edges from connectivity and calculate
     for z in range(conn.shape[0]):
@@ -102,20 +174,20 @@ def energy( x, conn, fvk=0.01, re=1.0, a=4.620981204 ):
         i, j = conn[z]
 
         # Get the positions and orientations and derivatives of normals
-        rij =  pos[j] - pos[i]
+        rij = pos[j] - pos[i]
         r = np.linalg.norm(rij)
-        rn = rij/r
+        rn = rij / r
 
         # Morse potential and derivatives
-        exp_1 = np.exp( -a*(r - re) )
+        exp_1 = np.exp(-a*(r - re))
         morse += exp_1*(exp_1 - 2)
 
         # Co-normality potential and derivatives
         m = ori[i] - ori[j]
-        normality += np.dot(m,m)/fvk
+        normality += np.dot(m, m)/fvk
 
         # Co-circularity potential and derivatives
-        n_dot_rn = np.dot( ori[i] + ori[j], rn )
-        circularity +=  ( n_dot_rn**2 )/fvk
+        n_dot_rn = np.dot(ori[i] + ori[j], rn)
+        circularity += (n_dot_rn**2) / fvk
 
     return morse, normality, circularity
--- ops/core/rotations.py
+++ ops/core/rotations.py
@@ -1,38 +1,63 @@
+from math import sin, cos, sqrt, asin, pi
 import numpy as np
-from numba import jit
+from numba import njit
 
-@jit(nopython=True,cache=True)
-def get_orientations(rotVec,orientations):
+
+@njit(cache=True)
+def get_orientations(rotvec, orientations):
     """ Convert Rotation Vectors to Orientations"""
-    for i in range( rotVec.shape[0] ):
-        r = rotVec[i]
-        R = np.linalg.norm(r) # Angle of rotation
+    for i in range(rotvec.shape[0]):
+        r0 = rotvec[i, 0]
+        r1 = rotvec[i, 1]
+        r2 = rotvec[i, 2]
+
+        R = sqrt(r0**2 + r1**2 + r2**2)
         # Check 0-angle case
         if R < 1e-10:
-            orientations[i] = np.array( [0.0, 0.0, 1.0] )
+            orientations[i, 0] = 0.0
+            orientations[i, 1] = 0.0
+            orientations[i, 2] = 1.0
         elif abs(R - np.pi) < 1e-10:
-            orientations[i] = np.array( [0.0, 0.0, -1.0] )
+            orientations[i, 0] = 0.0
+            orientations[i, 1] = 0.0
+            orientations[i, 2] = -1.0
         else:
-            # Make unit quaternion with angle R and axis along rotVec
-            q0 = np.cos( 0.5*R )
-            q1, q2, q3 = np.sin( 0.5*R )*r/R
-            orientations[i] = np.array([2*(q0*q2 + q1*q3), -2*(q0*q1 - q2*q3),
-                                        q0**2 - q1**2 - q2**2 + q3**2])
+            # Make unit quaternion with angle R and axis along rotvec
+            q0 = cos(0.5 * R)
+            sinR = sin(0.5 * R) / R
+            q1 = sinR * r0
+            q2 = sinR * r1
+            q3 = sinR * r2
+            orientations[i, 0] = 2*(q0*q2 + q1*q3)
+            orientations[i, 1] = -2*(q0*q1 - q2*q3)
+            orientations[i, 2] = q0**2 - q1**2 - q2**2 + q3**2
 
-@jit(nopython=True,cache=True)
-def get_rotation_vectors(orientations,rotVec):
+
+@njit(cache=True)
+def get_rotation_vectors(orientations, rotvec):
     """ Convert Orientations to Rotation Vectors """
     for i in range(orientations.shape[0]):
-        p0, p1, p2 = orientations[i]
+        p0 = orientations[i, 0]
+        p1 = orientations[i, 1]
+        p2 = orientations[i, 2]
         # Cross-product of ptNormal with z-axis.
-        cross_prod = np.array( [-p1, p0, 0.0] )
-        cross_prod_norm = np.sqrt( p1**2 + p0**2 )
+        cross_prod0 = -p1
+        cross_prod1 = p0
+        cross_prod2 = 0.0
+        cross_prod_norm = sqrt(p1**2 + p0**2)
         # Check if the orientation is parallel or anti-parallel to z-axis
         if cross_prod_norm < 1e-10:
-            axis = np.array([ 1.0, 0.0, 0.0 ]) #choose the x-axis
-            angle = 0.0 if p2 > 0.0 else np.pi
+            axis0 = 1.0
+            axis1 = 0.0
+            axis2 = 0.0  # choose the x-axis
+            angle = 0.0 if p2 > 0.0 else pi
         else:
-            angle = np.arcsin( cross_prod_norm )
-            angle = (np.pi - angle) if p2 < 0.0 else angle
-            axis = cross_prod/cross_prod_norm
-        rotVec[i] = angle*axis
+            angle = asin(cross_prod_norm)
+            angle = (pi - angle) if p2 < 0.0 else angle
+            axis0 = cross_prod0/cross_prod_norm
+            axis1 = cross_prod1/cross_prod_norm
+            axis2 = cross_prod2/cross_prod_norm
+
+        rotvec[i, 0] = angle * axis0
+        rotvec[i, 1] = angle * axis1
+        rotvec[i, 2] = angle * axis2
